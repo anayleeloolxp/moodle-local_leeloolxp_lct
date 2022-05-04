e641ecd0f952167da64acf7457fbd78d9552efe9
Anay Infowind <anay.infowind@gmail.com>
2022-05-04 18:52:59 +0530

    Updated version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021062201;
+$plugin->version = 2022050500;
 $plugin->requires = 2013051500;
 $plugin->release = '1.0.0';
 $plugin->maturity = MATURITY_STABLE;
